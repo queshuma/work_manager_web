108d40346d8414ee64f48a224e6749d5f8098235
chentao <chentao@amberdata.cn>
2025-04-06 00:36:45 +0800

    improve #000 优化用户分页查询问题

--- src/views/admin/User.vue
+++ src/views/admin/User.vue
@@ -19,116 +19,15 @@
           v-model="dialog"
           max-width="500px"
         >
-          <template v-slot:activator="{ props }">
-            <v-btn
-              class="mb-2"
-              color="primary"
-              v-bind="props"
-            >
-              New Item
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col
-                    cols="12"
-                    md="4"
-                    sm="6"
-                  >
-                    <v-text-field
-                      v-model="editedItem.id"
-                      label="编号"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="4"
-                    sm="6"
-                  >
-                    <v-text-field
-                      v-model="editedItem.username"
-                      label="用户名"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="4"
-                    sm="6"
-                  >
-                    <v-text-field
-                      v-model="editedItem.createTime"
-                      label="创建时间"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="4"
-                    sm="6"
-                  >
-                    <v-text-field
-                      v-model="editedItem.address"
-                      label="用户地址"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="4"
-                    sm="6"
-                  >
-                    <v-text-field
-                      v-model="editedItem.role"
-                      label="角色"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="4"
-                    sm="6"
-                  >
-                    <v-text-field
-                      v-model="editedItem.online"
-                      label="是否上线"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col
-                    cols="12"
-                    md="4"
-                    sm="6"
-                  >
-                    <v-text-field
-                      v-model="editedItem.deleted"
-                      label="是否删除"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="blue-darken-1"
-                variant="text"
-                @click="close"
-              >
-                Cancel
-              </v-btn>
-              <v-btn
-                color="blue-darken-1"
-                variant="text"
-                @click="save"
-              >
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
         </v-dialog>
+        <template v-slot:item.status="{ value }">
+          <v-chip
+            :border="`${getColor(value)} thin opacity-25`"
+            :color="getColor(value)"
+            :text="value"
+            size="x-small"
+          ></v-chip>
+        </template>
         <v-dialog v-model="dialogDelete" max-width="500px">
           <v-card>
             <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
@@ -192,8 +91,7 @@ const headers = ref([
   { title: '创建时间:', key: 'createTime' },
   { title: '地址:', key: 'address' },
   { title: '角色:', key: 'role' },
-  { title: '是否上线:', key: 'online' },
-  { title: '是否删除:', key: 'deleted' },
+  { title: '当前状态:', key: 'status' },
 ])
 const desserts = ref([])
 const editedIndex = ref(-1)
@@ -203,8 +101,7 @@ const editedItem = ref({
   createTime: 0,
   address: 0,
   role: 0,
-  online:  false,
-  deleted: false
+  status: 0,
 })
 const defaultItem = ref({
   id: '',
@@ -212,12 +109,13 @@ const defaultItem = ref({
   createTime: 0,
   address: 0,
   role: 0,
-  online:  false,
-  deleted: false
+  status: 0,
 })
-// const formTitle = computed(() => {
-//   return editedIndex.value === -1 ? 'New Item' : 'Edit Item'
-// })
+const statusOptions= [
+  { text: '下线', value: 0 },
+  { text: '上线', value: 1 },
+  { text: '删除', value: 2 }
+]
 function initialize () {
   link("/User/page", 'POST',{}, {
     'page': 0,
@@ -228,76 +126,6 @@ function initialize () {
     }
   })
   desserts.value = [
-    {
-      name: 'Frozen Yogurt',
-      calories: 159,
-      fat: 6,
-      carbs: 24,
-      protein: 4,
-    },
-    {
-      name: 'Ice cream sandwich',
-      calories: 237,
-      fat: 9,
-      carbs: 37,
-      protein: 4.3,
-    },
-    {
-      name: 'Eclair',
-      calories: 262,
-      fat: 16,
-      carbs: 23,
-      protein: 6,
-    },
-    {
-      name: 'Cupcake',
-      calories: 305,
-      fat: 3.7,
-      carbs: 67,
-      protein: 4.3,
-    },
-    {
-      name: 'Gingerbread',
-      calories: 356,
-      fat: 16,
-      carbs: 49,
-      protein: 3.9,
-    },
-    {
-      name: 'Jelly bean',
-      calories: 375,
-      fat: 0,
-      carbs: 94,
-      protein: 0,
-    },
-    {
-      name: 'Lollipop',
-      calories: 392,
-      fat: 0.2,
-      carbs: 98,
-      protein: 0,
-    },
-    {
-      name: 'Honeycomb',
-      calories: 408,
-      fat: 3.2,
-      carbs: 87,
-      protein: 6.5,
-    },
-    {
-      name: 'Donut',
-      calories: 452,
-      fat: 25,
-      carbs: 51,
-      protein: 4.9,
-    },
-    {
-      name: 'KitKat',
-      calories: 518,
-      fat: 26,
-      carbs: 65,
-      protein: 7,
-    },
   ]
 }
 function history (item) {
@@ -347,5 +175,11 @@ watch(dialog, val => {
 watch(dialogDelete, val => {
   val || closeDelete()
 })
+function getColor (status) {
+  if (status = 0) return 'error'
+  else if(status == 1) return  'info'
+  else if (status = 2) return 'warning'
+  else return 'success'
+}
 initialize()
 </script>

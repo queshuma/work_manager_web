eba2cc3b1349de1429e39e49bd288beb15299c66
shuzhi <1428767439@163.com>
2025-04-11 00:31:33 +0800

    improve #000 测试minIO文件上传功能

--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -18,7 +18,8 @@
     console.log(event.target.files[0]);
     const formData = new FormData();
     formData.append("file", event.target.files[0]);
-    link("/User/uploadImage", 'POST',formData, {}, {}, ).then(response => {
+    // link("/User/uploadImage", 'POST',formData, {}, {}, ).then(response => {
+    link("/User/uploadFile", 'POST',formData, {}, {}, ).then(response => {
       if (response.status === 200) {
         console.log(response.data.url);
         imageUrl.value = "http://localhost:8181/" + response.data.name;
